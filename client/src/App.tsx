901a2734e6ba918ebd267eca7c016083d6630ed0
Igor Stepanov <stipyk1309@gmail.com>
2023-02-08 17:42:41 +0300

    add header, update styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { logo } from './assets/icons';
+import { Link } from 'react-router-dom';
 
 export const App = () => {
-  return <div>Test</div>;
+  return (
+    <>
+      <header className="w-full flex justify-between items-center sm:px-10 px-5 py-5 border-b border-b-[#383636] bg-[#181818]">
+        <Link to={'/'}>
+          <img className="w-32 object-contain" src={logo} alt="logo" />
+        </Link>
+        <Link
+          className=" bg-[#37a051] text-white px-8 py-3 rounded-md"
+          to={'/create-post'}
+        >
+          Create
+        </Link>
+      </header>
+      <div className="bg-[#181818] h-72"></div>
+    </>
+  );
 };
