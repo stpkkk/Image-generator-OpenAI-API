145e67d5130f4ef62a3d0f4eca5cfe1074942630
Igor Stepanov <stipyk1309@gmail.com>
2023-02-18 16:41:09 +0300

    utils fix

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,9 +1,10 @@
 import { surpriseMePrompts } from '../constants';
 
-// eslint-disable-next-line consistent-return
-export function getRandomPrompt(prompt: string): void {
+export function getRandomPrompt(prompt: string): string {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
 
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
 }
