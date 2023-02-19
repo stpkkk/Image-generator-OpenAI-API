f269d1a7a1107ab88b60377f3933fe9ffe917234
Igor Stepanov <stipyk1309@gmail.com>
2023-02-19 13:06:31 +0300

    server add Routes, index.ts update

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,9 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-import { connectDB } from "./mongodb/connect";
-
-// import connectDB from './mongodb/connect.js';
-// import postRoutes from './routes/postRoutes.js';
-// import dalleRoutes from './routes/dalleRoutes.js';
+import connectDB from "./mongodb/connect";
+import postRoutes from "./routes/postRoutes";
+import dalleRoutes from "./routes/dalleRoutes";
 
 dotenv.config();
 
@@ -15,9 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-
-// app.use('/api/v1/post', postRoutes);
-// app.use('/api/v1/dalle', dalleRoutes);
+//Routes
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from DALL-E!");
@@ -25,7 +23,7 @@ app.get("/", (req, res) => {
 
 const startServer = async () => {
   try {
-    // connectDB(process.env.MONGODB_URL);
+    connectDB(process.env.MONGODB_URL!);
     app.listen(8080, () => console.log("Server started on port 8080"));
   } catch (error) {
     console.log(error);
